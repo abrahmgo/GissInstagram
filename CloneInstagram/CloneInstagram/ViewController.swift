547078e1e25e18a3a0243a8f4c6cf712d321c1d6
Andres Bonilla Gomez <abraham.go@outlook.com>
2021-07-26 20:46:57 -0500

    Init project

---
+++ CloneInstagram/CloneInstagram/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  CloneInstagram
+//
+//  Created by Andrés Bonilla Gómez on 26/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
