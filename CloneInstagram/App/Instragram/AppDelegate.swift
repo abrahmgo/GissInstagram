5a623b25585162b5981bf45406f28cd7f35e7b01
Gissel Santiago <egissel081522@gmail.com>
2021-08-31 19:42:47 -0500

    Add userDefaults for the first view, correction

--- CloneInstagram/App/Instragram/AppDelegate.swift
+++ CloneInstagram/App/Instragram/AppDelegate.swift
@@ -14,12 +14,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        window = UIWindow()
-        let navigationController = UINavigationController(rootViewController: LoginViewController())
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-        
-        return true
+            
+        if !UserDefaultForViews.isFirstView {
+            
+            UserDefaultForViews.isFirstView = true
+            
+                window = UIWindow()
+                let navigationController = UINavigationController(rootViewController: LoginViewController())
+                window?.rootViewController = navigationController
+                window?.makeKeyAndVisible()
+                
+                return true
+
+        } else {
+            window = UIWindow()
+            let navigationController = UINavigationController(rootViewController: TableViewController())
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+            
+            return true
+        }
     }
 }
